e1508b483305c5258d3c116f184cfa8da9db5f6d
Ilya <ilya.kurymshin@steiza.com>
2024-10-15 16:28:43 +0300

    add uuid book

--- frontend/src/components/BookList/BookList.jsx
+++ frontend/src/components/BookList/BookList.jsx
@@ -11,7 +11,7 @@ function BookList(props) {
       ) : (
         <ul>
           {books.map((book, i) => (
-            <li key={i}>
+            <li key={book.id}>
               <div className="book-info">
                 {++i}. {book.title} by <strong>{book.author}</strong>
               </div>
